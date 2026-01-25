5b254addae4d224f3828cc1f02c4fbc8af52fb0e
AndrewFinch270 <andrew.finch270@gmail.com>
2026-01-25 00:01:02 +0000

    pipeline

--- 01-docker/pipeline/pipeline.py
+++ 01-docker/pipeline/pipeline.py
@@ -6,7 +6,8 @@ print(f"Running pipeline for month {month}")
 
 import pandas as pd
 
-df=pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df=pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df['month'] = month
 print(df.head())
 
 df.to_parquet(f"output_month_{sys.argv[1]}.parquet")
